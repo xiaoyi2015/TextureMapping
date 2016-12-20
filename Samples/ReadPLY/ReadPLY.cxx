38d1b3a3ddf4839d9f7349c71ef1ad8a93702236
xiaoyi2015 <zlnxiaoyi2016@gmail.com>
2016-12-20 20:37:21 +0800

    init and commit

---
+++ Samples/ReadPLY/ReadPLY.cxx
@@ -0,0 +1,44 @@
+#include <vtkPolyData.h>
+#include <vtkPLYReader.h>
+#include <vtkSmartPointer.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+
+int main ( int argc, char *argv[] )
+{
+  
+  std::string inputFilename = "C://Users//zln//Desktop//TextureMapping//bun.ply";
+
+  vtkSmartPointer<vtkPLYReader> reader =
+    vtkSmartPointer<vtkPLYReader>::New();
+  reader->SetFileName ( inputFilename.c_str() );
+
+  // Visualize
+  vtkSmartPointer<vtkPolyDataMapper> mapper =
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+  mapper->SetInputConnection(reader->GetOutputPort());
+
+  vtkSmartPointer<vtkActor> actor =
+    vtkSmartPointer<vtkActor>::New();
+  actor->SetMapper(mapper);
+
+  vtkSmartPointer<vtkRenderer> renderer =
+    vtkSmartPointer<vtkRenderer>::New();
+  vtkSmartPointer<vtkRenderWindow> renderWindow =
+    vtkSmartPointer<vtkRenderWindow>::New();
+  renderWindow->AddRenderer(renderer);
+  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  renderWindowInteractor->SetRenderWindow(renderWindow);
+
+  renderer->AddActor(actor);
+  renderer->SetBackground(0.1804,0.5451,0.3412); // Sea green
+
+  renderWindow->Render();
+  renderWindowInteractor->Start();
+
+  return EXIT_SUCCESS;
+}
